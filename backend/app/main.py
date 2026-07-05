41841aa9828a3db9201aba369071a3ee1b3a6084
Senuka Wijerathna <senukaw615@gmail.com>
2026-07-06 00:26:01 +0530

    Add the Employee model, schema and DB connection setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.routers import job
+from app.routers import employee
 
 app = FastAPI(title="PrimeAutocare API")
 
 app.include_router(job.router)
+app.include_router(employee.router)
+
 
 @app.get("/")
 def root():
